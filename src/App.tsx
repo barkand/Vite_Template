df481f3a4bff72935a9f4eae10891f882221139e
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-20 22:50:46 +0330

    Add head tag with react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { HashRouter as Router } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import Layout from "./app/layout";
 
 export default function App() {
   return (
     <>
+      <Helmet>
+        <title>{import.meta.env.VITE_COMPANY_NAME}</title>
+        <meta name="description" content={import.meta.env.VITE_COMPANY_NAME} />
+      </Helmet>
       <Router>
         <Layout />
       </Router>
