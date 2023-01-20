5b572a1cc22c8dcf1a1cca8963a2671a987868d2
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-21 00:58:15 +0330

    Add other props

--- src/core/components/mui/iconButton.tsx
+++ src/core/components/mui/iconButton.tsx
@@ -1,14 +1,14 @@
 import { IconButton } from "@mui/material";
 
 export default function IconButtonMUI(props: any) {
-  const { children, onClick }: { children: React.ReactNode; onClick: any } =
-    props;
+  const { children, onClick, ...other } = props;
 
   return (
     <IconButton
       sx={{ ...props.sx, width: "40px", height: "40px" }}
       aria-label={props.label}
       onClick={onClick}
+      {...other}
     >
       {children}
     </IconButton>
