e4f267cd5ac890f72a362abe54b32246ba2e4966
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-20 21:32:13 +0330

    Add ImportMetaEnv IntelliSense

---
+++ src/env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DEFAULT_COPY_RIGHT: string;
+  readonly VITE_DEFAULT_MODE: string;
+  readonly VITE_DEFAULT_COLOR: string;
+  readonly VITE_DEFAULT_LANGUAGE: string;
+  readonly VITE_Background_light_primary: string;
+  readonly VITE_Background_light_secondary: string;
+  readonly VITE_Background_light_tertiary: string;
+  readonly VITE_Background_dark_primary: string;
+  readonly VITE_Background_dark_secondary: string;
+  readonly VITE_Background_dark_tertiary: string;
+  readonly VITE_COMPANY_NAME: string;
+  readonly VITE_COMPANY_SHORT_NAME: string;
+  readonly VITE_COMPANY_DESCRIPTION: string;
+  readonly VITE_CLIENT_PATH: string;
+  readonly VITE_SERVER_PATH: string;
+  readonly VITE_UPLOAD_PATH: string;
+  readonly VITE_UPLOAD_FOLDER: string;
+  readonly VITE_REFRESH_SECRET_KEY: string;
+  readonly VITE_GAS_LIMIT: string;
+  readonly VITE_INFURA_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
