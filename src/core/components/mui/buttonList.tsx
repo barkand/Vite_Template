a457654c5cd8fd8d21d855ff95afe2d6d73c3653
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-21 21:48:33 +0330

    Add className

--- src/core/components/mui/buttonList.tsx
+++ src/core/components/mui/buttonList.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
-import { ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from "@mui/material";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+} from "@mui/material";
 
 export default function ButtonList(props: any) {
   const { to, name, icon, onclick } = props;
   return (
     <>
-      <ListItem component={Link} to={to} onClick={onclick} disablePadding>
+      <ListItem
+        component={Link}
+        to={to}
+        onClick={onclick}
+        disablePadding
+        className={props.className}
+      >
         <ListItemButton>
           <ListItemIcon>{icon}</ListItemIcon>
           <ListItemText primary={name} />
