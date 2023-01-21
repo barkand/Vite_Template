8daa497c06c847e6680b1e1ae99bb2b27a8bd71c
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-21 17:57:58 +0330

    Move Aos Libs to Effect Folder

---
+++ src/core/components/effect/index.ts
@@ -0,0 +1,4 @@
+import "aos/dist/aos.css";
+import AOS from "aos";
+
+export default AOS;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "aos/dist/aos.css";
-import AOS from "aos";
-AOS.init();
+import { TiltEffect } from "./core/components";
+TiltEffect.init();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
