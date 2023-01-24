85243821cebef4a4a86f1e3034f753abe8f010e7
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-24 22:11:27 +0330

    Add PieChart

---
+++ src/core/components/charts/pie.tsx
@@ -0,0 +1,62 @@
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}: any) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+export default function PieReChart({ data }: { data: any }) {
+  return (
+    <div
+      style={{
+        width: "auto",
+        height: "100%",
+        direction: "ltr",
+        margin: "10px",
+      }}
+    >
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry: any, index: any) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={data[index % data.length].color}
+              />
+            ))}
+          </Pie>
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
