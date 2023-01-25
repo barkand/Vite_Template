b3f637afbe6b52fde8afa0e70711b5ed75f55bb3
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-25 14:25:33 +0330

    Add Post Api Libs

---
+++ src/core/libs/api/index.ts
@@ -0,0 +1,32 @@
+const PostAuth = async (params: any, route: string) => {
+  let _result = await fetch(`${import.meta.env.VITE_SERVER_PATH}${route}`, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({ params }),
+  })
+    .then((res) => res.json())
+    .then((d) => d)
+    .catch((err) => {
+      return { code: 500 };
+    });
+
+  return { code: 200, status: _result.data };
+};
+
+const Post = async (params: any, route: string) => {
+  let _result = await fetch(`${import.meta.env.VITE_SERVER_PATH}${route}`, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ params }),
+  })
+    .then((res) => res.json())
+    .then((d) => d)
+    .catch((err) => {
+      return { code: 500 };
+    });
+
+  return { code: 200, items: _result.data };
+};
+
+export { PostAuth, Post };
--- src/core/libs/index.ts
+++ src/core/libs/index.ts
@@ -1,2 +1,3 @@
 export { importPhoto } from "./image";
 export { default as logger } from "./log";
+export { PostAuth  as PostAuthApi , Post as PostApi } from "./api"
