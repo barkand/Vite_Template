c593b2009b7057fb24a07533a9532ed407783d23
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-29 23:06:00 +0330

    Use Custom shortUser

--- src/admin/components/profile/gadget/mini.tsx
+++ src/admin/components/profile/gadget/mini.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { RoutesTypeEnum } from "../../../../core/constant";
 import { PublicContext } from "../../../../core/context";
 import { Avatar, Label, Divider, Rating } from "../../../../core/components";
+import { shortUser } from "../../../../admin/components/security/Authentication";
 
 export default function MiniProfileGadget({ onClose }: { onClose: any }) {
   const { publicCtx }: { publicCtx: any } = React.useContext(PublicContext);
@@ -50,12 +51,7 @@ export default function MiniProfileGadget({ onClose }: { onClose: any }) {
               size="caption"
               sx={{ paddingTop: "10px", paddingBottom: "25px" }}
             >
-              {publicCtx.user.user_id.substr(0, 12) +
-                "..." +
-                publicCtx.user.user_id.substr(
-                  publicCtx.user.user_id.length - 13,
-                  publicCtx.user.user_id.length
-                )}
+              {shortUser()}
             </Label>
           </div>
           <Divider />
