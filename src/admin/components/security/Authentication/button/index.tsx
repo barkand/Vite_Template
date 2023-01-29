e41e886ea32ace858cf1bfddfa5a20e0d27acfaf
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-29 23:05:20 +0330

    Add Conditional Auth

--- src/admin/components/security/Authentication/button/index.tsx
+++ src/admin/components/security/Authentication/button/index.tsx
@@ -1,78 +1,16 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
+import { AuthTypeEnum } from "../../../../../core/constant";
 
-import { PublicContext } from "../../../../../core/context";
-import { IconButton, Tip } from "../../../../../core/components";
-import { LockIcon, LockOpenIcon } from "../../../../../core/icon";
-
-import useUser from "../hook/use-user";
-import { Login, Logout } from "./loginLogout";
+import { WalletButton } from "../features/wallet";
+import { MailButton } from "../features/mail";
 
 export default function AuthButton() {
-  const { t } = useTranslation(["admin"]);
-  const { publicCtx, setPublicCtx }: any = React.useContext(PublicContext);
-  const [loaded, setLoaded] = React.useState<boolean>(false);
-  const { _, loading, loggedOut, mutate }: any = useUser();
-
-  const authClick = () => {
-    const auth = async () => {
-      let _result: any;
-      if (!loggedOut === true) {
-        _result = await Logout();
-        mutate(() => null);
-      } else {
-        _result = await Login(publicCtx.device.isMobile);
-        mutate();
-      }
-
-      setPublicCtx({
-        ...publicCtx,
-        user: _result.user,
-        alert: {
-          ..._result.alert,
-          message: t(_result.alert.message),
-        },
-      });
-    };
-
-    auth();
-  };
-
-  React.useEffect(() => {
-    if (!loaded) {
-      setLoaded(true);
-      return;
-    }
-
-    if (!(!loggedOut === true) && publicCtx.user.connected) {
-      const logout = async () => {
-        let _result: any = await Logout();
-        setPublicCtx({
-          ...publicCtx,
-          user: _result.user,
-          alert: {
-            ..._result.alert,
-            message: t(_result.alert.message),
-          },
-        });
-      };
-      logout();
-    }
-  }, [loggedOut]);
-
   return (
     <>
-      <IconButton onClick={authClick} className="step-auth">
-        {!loggedOut === true ? (
-          <Tip title={t("logout")}>
-            <LockOpenIcon />
-          </Tip>
-        ) : (
-          <Tip title={t("login")}>
-            <LockIcon />
-          </Tip>
-        )}
-      </IconButton>
+      {import.meta.env.VITE_AUTH_TYPE === `${AuthTypeEnum.Wallet}` ? (
+        <WalletButton />
+      ) : (
+        <MailButton />
+      )}
     </>
   );
 }
