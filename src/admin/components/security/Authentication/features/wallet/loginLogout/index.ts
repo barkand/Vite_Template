123decaa26d205fa0f8ffa8c7f15c92d6dc33d14
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-29 23:03:05 +0330

    Remove Class of Login Logout Wallet

--- src/admin/components/security/Authentication/features/wallet/loginLogout/index.ts
+++ src/admin/components/security/Authentication/features/wallet/loginLogout/index.ts
@@ -6,36 +6,34 @@ import MobileWallet from "../connect/mobile";
 import WebWallet from "../connect/web";
 import { Disconnect } from "../libs/web3";
 
-class Auth {
-  Login = async (isMobile: boolean) => {
-    let _walletType = isMobile || !(window as any).ethereum ? "app" : "plugin";
-    localStorage.setItem("walletType", `${_walletType}`);
+const Login = async (isMobile: boolean) => {
+  let _walletType = isMobile || !(window as any).ethereum ? "app" : "plugin";
+  localStorage.setItem("walletType", `${_walletType}`);
 
-    let _result =
-      _walletType === "app" ? await MobileWallet() : await WebWallet();
+  let _result =
+    _walletType === "app" ? await MobileWallet() : await WebWallet();
 
-    return _result;
+  return _result;
+};
+
+const Logout = async () => {
+  localStorage.removeItem("userId");
+  localStorage.removeItem("netId");
+  await Disconnect();
+
+  let _result = await PostAuthApi({}, "admin/logout");
+  let _alert: any = {
+    open: true,
+    message: _result.code === 200 ? "LogoutSuccess" : "LogoutFailed",
+    severity:
+      _result.code === 200 ? StatusTypeEnum.Warning : StatusTypeEnum.Error,
   };
 
-  Logout = async () => {
-    localStorage.removeItem("userId");
-    localStorage.removeItem("netId");
-    await Disconnect();
-
-    let _result = await PostAuthApi({}, "admin/logout");
-    let _alert: any = {
-      open: true,
-      message: _result.code === 200 ? "LogoutSuccess" : "LogoutFailed",
-      severity:
-        _result.code === 200 ? StatusTypeEnum.Warning : StatusTypeEnum.Error,
-    };
-
-    return {
-      ..._result,
-      user: DefaultUser,
-      alert: _alert,
-    };
+  return {
+    ..._result,
+    user: DefaultUser,
+    alert: _alert,
   };
-}
+};
 
-export default new Auth();
+export { Login, Logout };
